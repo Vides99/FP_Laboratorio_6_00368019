8d9401f34fa5ddbf4304e4dcb391bde64d9f702d
Vides99 <54686533+Vides99@users.noreply.github.com>
2019-10-16 11:51:30 -0600

    ejercicio8

---
+++ ejer8.py
@@ -0,0 +1,3 @@
+numeroAmultiplicar = int(input("Ingrese el numero del cual quiere conocer la tabla (dicha tabla sera del 1 hasta el 10)"))
+for i in range (1,11):
+    print(numeroAmultiplicar, " * ", i, " = ", (numeroAmultiplicar * i))
